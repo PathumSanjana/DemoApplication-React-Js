05f0392f9f88083fe1578714f1f3e3806684c8be
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-03-17 01:59:44 +0530

    add dynamic components

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,10 +2,21 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+
+//const topic = "My favourite food is kottu"
+//const item = " and rice"
+
+const a=10;
+const b=20;
+const c=a+b;
+//const foodprices = {kottu:"250", rice:"350"} - we can't take objects and boolean values as react child
+
   return (
     <div>
         <div className="topicBox">
-            <span className="text">This is the topic</span>
+            <span className="text">{c}{alert("Hellow")}{[20,45,34,67]}</span>
+            <span className="text">{[20,45,34,67]}</span>
+            
         </div>
     </div>
   )
